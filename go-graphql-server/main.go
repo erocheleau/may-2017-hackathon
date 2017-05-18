c4e1757eeef7f94da34a6c6f9a4958a3f8c9faaf
André Thériault <atheriault2@coveo.com>
2017-05-18 15:58:49 -0400

    yayyy

--- go-graphql-server/main.go
+++ go-graphql-server/main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/graphql-go/graphql"
-	gqlhandler "github.com/graphql-go/graphql-go-handler"
+	"fmt"
 )
 
-var queryType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "Query",
-	Fields: graphql.Fields{
-		"latestPost": &graphql.Field{
-			Type: graphql.String,
-			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "Hello World!", nil
-			},
-		},
-	},
-})
-
-var Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
-	Query: queryType,
-})
-
 func main() {
-	h := gqlhandler.New(&gqlhandler.Config{
-		Schema: &Schema,
-		Pretty: true,
-	})
-
-	http.Handle("/graphql", h)
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("%+v\n", DoQuery("@uri"))
 }
