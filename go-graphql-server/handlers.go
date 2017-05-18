65de1a0150d09e2ec36df97ae0ad915a53db017e
erocheleau <etienne.rocheleau@gmail.com>
2017-05-18 19:42:58 -0400

    Did the query post

--- go-graphql-server/handlers.go
+++ go-graphql-server/handlers.go
@@ -9,6 +9,10 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
+type QueryPost struct {
+	Query string `json:"query"`
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	w.Write([]byte("POST/GET on /graphql"))
@@ -44,5 +48,33 @@ func GraphQLGETHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func GraphQLPOSTHandler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	var queryPost QueryPost
+	err := decoder.Decode(&queryPost)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Errorf("Error parsing query: ", err.Error())
+		return
+	}
 
+	defer r.Body.Close()
+
+	w.Header().Set("Content-Type", "application/json; chartype: utf-8")
+	w.WriteHeader(200)
+
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: queryPost.Query,
+	})
+
+	if result.Errors != nil {
+		fmt.Fprintf(w, "%v", result.Errors)
+	}
+
+	b, err := json.Marshal(result.Data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Fprint(w, string(b))
 }
